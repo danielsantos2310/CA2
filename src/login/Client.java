8e4373dea7d1515623fd80333421dc56812453e9
danielsantos2310 <71696181+danielsantos2310@users.noreply.github.com>
2020-12-10 23:42:58 +0000

    list picker
    many mistake

--- src/login/Client.java
+++ src/login/Client.java
@@ -7,6 +7,7 @@ package login;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -24,60 +25,108 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
 
 /**
  *
  * @author Daniel Santos
  */
-class Client extends JFrame implements ActionListener {
+class Client extends JFrame implements ActionListener{
+
+//    private JList leftlist;
+//    private JList rightlist;
+//    private JButton movebutton;
+    private static String[] time = {"08:00", "09:00", "09:00", "09:00", "09:00", "09:00"};
 
     public Client() {
-        super("Client Area");
-        JPanel clientest = new JPanel();
-        JLabel clientitle = new JLabel("Pick a Date");
-        clientitle.setAlignmentX(TOP_ALIGNMENT);
-        clientest.add(clientitle);
-        this.add(clientest);
-        GridLayout clientlayout = new GridLayout(1, 2);
-        this.setLayout(clientlayout);
-        this.setSize(600, 300);
         this.setVisible(true);
+        this.setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        JPanel timepanel = new JPanel();
+        this.add(timepanel);
+       JList leftlist = new JList(time);
+       timepanel.add(leftlist);
+        leftlist.setVisibleRowCount(3);
+        leftlist.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        JScrollPane js = new JScrollPane();
+        timepanel.add(js, leftlist);
+        timepanel.add(js);
+        
+        JList rightlist = new JList();
+        rightlist.setVisibleRowCount(3);
+        rightlist.setFixedCellWidth(100);
+        rightlist.setFixedCellHeight(30);
+        rightlist.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        JScrollPane jsr = new JScrollPane();
+        this.add(jsr, rightlist);
 
-        //Pick  date
-        Date date = new Date();
-    
-        // time pick
-        JPanel timepanel =new JPanel();
-//        BoxLayout timebox = new BoxLayout(timepanel, BoxLayout.Y_AXIS);
-//        timepanel.setLayout(timebox);
-        this.add(timepanel, BorderLayout.CENTER);
-        JLabel clientdate = new JLabel("Pick a Time");
-        timepanel.add(clientdate);
-        String labels[] = {"08:00", "09:00", "10:00", "11:00", "12:00", "14:00", "15:00", "16:00", "17:00", "18:00"};
-        JList list = new JList(labels);
-        JScrollPane scrollPane = new JScrollPane(list);
-        Container contentPane = this.getContentPane();
-        timepanel.add(scrollPane);
-        scrollPane.setMaximumSize(scrollPane.getPreferredSize());
+        JButton movebutton = new JButton("add Day");
+        movebutton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                rightlist.setListData((Object[]) leftlist.getSelectedValue());
+            }
+        });
+        add(movebutton);
         
-        //        
-        //time pick button
-//       
-        JButton pickdaybutton = new JButton("Pick a Time");
-        timepanel.add(pickdaybutton);
-//        this.add(pickdaybutton, BorderLayout.PAGE_START);
-//        pickdaybutton.setBounds(50, 50, 30, 30);
-        //pickdaybutton.setMaximumSize(pickdaybutton.getPreferredSize());
-        
-        this.validate();
-        this.repaint();
+//        
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
+
+//
+//    public Client() {
+//        super("Client Area");
+//        JPanel clientest = new JPanel();
+//        JLabel clientitle = new JLabel("Pick a Date");
+//        clientitle.setAlignmentX(TOP_ALIGNMENT);
+//        clientest.add(clientitle);
+//        this.add(clientest);
+//        GridLayout clientlayout = new GridLayout(1, 2);
+//        this.setLayout(clientlayout);
+//        this.setSize(600, 300);
+//        this.setVisible(true);
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//
+//        //Pick  date
+//        Date date = new Date();
+//    
+//        // time pick
+//        JPanel timepanel =new JPanel();
+//        BoxLayout timebox = new BoxLayout(timepanel, BoxLayout.Y_AXIS);
+//        timepanel.setLayout(timebox);
+//        this.add(timepanel, BorderLayout.CENTER);
+//        JLabel clientdate = new JLabel("Pick a Time");
+//        timepanel.add(clientdate);
+//        String labels[] = {"08:00", "09:00", "10:00", "11:00", "12:00", "14:00", "15:00", "16:00", "17:00", "18:00"};
+//        JList list = new JList(labels);
+//        JScrollPane scrollPane = new JScrollPane(list);
+//        Container contentPane = this.getContentPane();
+//        timepanel.add(scrollPane);
+//        scrollPane.setMaximumSize(scrollPane.getPreferredSize());
+//        
+//        //        
+//        //time pick button
+////       
+//        JButton pickdaybutton = new JButton("Pick a Time");
+//        timepanel.add(pickdaybutton);
+////        this.add(pickdaybutton, BorderLayout.PAGE_START);
+////        pickdaybutton.setBounds(50, 50, 30, 30);
+//        //pickdaybutton.setMaximumSize(pickdaybutton.getPreferredSize());
+//        
+//        this.validate();
+//        this.repaint();
+//
+//    }
+//
+//    @Override
+//    public void actionPerformed(ActionEvent e) {
+//
+//    }
+
