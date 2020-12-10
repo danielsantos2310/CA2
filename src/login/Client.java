c4ae7b8ed9d4646951a264dd11cbe398051565e6
danielsantos2310 <71696181+danielsantos2310@users.noreply.github.com>
2020-12-10 00:58:54 +0000

    button size
    nao consigo alterar o tamanho do butao

--- src/login/Client.java
+++ src/login/Client.java
@@ -10,6 +10,14 @@ import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -20,12 +28,12 @@ import javax.swing.JScrollPane;
  *
  * @author Daniel Santos
  */
-class Client extends JFrame implements ActionListener{
+class Client extends JFrame implements ActionListener {
 
     public Client() {
         super("Client Area");
         JPanel clientest = new JPanel();
-        JLabel clientitle = new JLabel("Pick a Date & Time");
+        JLabel clientitle = new JLabel("Pick a Date");
         clientitle.setAlignmentX(TOP_ALIGNMENT);
         clientest.add(clientitle);
         this.add(clientest);
@@ -35,21 +43,38 @@ class Client extends JFrame implements ActionListener{
         this.setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Date and time pick
-        JLabel clientdate = new JLabel("test");
+        //Pick  date
+        Date date = new Date();
+    
+        // time pick
+        JPanel timepanel =new JPanel();
+        BoxLayout timebox = new BoxLayout(timepanel, BoxLayout.Y_AXIS);
+        timepanel.setLayout(timebox);
+        this.add(timepanel, BorderLayout.EAST);
+//        BoxLayout timebox = new BoxLayout ();
+        JLabel clientdate = new JLabel("Pick a Time");
         this.add(clientdate);
         String labels[] = {"08:00", "09:00", "10:00", "11:00", "12:00", "14:00", "15:00", "16:00", "17:00", "18:00"};
         JList list = new JList(labels);
         JScrollPane scrollPane = new JScrollPane(list);
         Container contentPane = this.getContentPane();
         contentPane.add(scrollPane, BorderLayout.CENTER);
-//      
+//        scrollPane.setMaximumSize(scrollPane.getPreferredSize());
+        
+        //time pick button
+//       
+        JButton pickdaybutton = new JButton("Pick a Time");
+        this.add(pickdaybutton);
+        //pickdaybutton.setMaximumSize(pickdaybutton.getPreferredSize());
+        
+        this.validate();
+        this.repaint();
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+
     }
 
 }
